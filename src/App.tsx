bb22156a9df5b4b84a2e5ad9e07c9cd3e09ca7f8
Akshaj Mody <akshajmody@gmail.com>
2021-05-30 21:58:43 -0700

    add list and add to list functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List';
 
-interface IState {
-  people: {
-    name: string;
-    age: number;
-    url: string;
-    note?: string;
-  }[];
-}
+import { IState } from './types/People';
 
 function App() {
-  const [people, setPeople] = useState<IState['people']>([]);
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: 'LeBron James',
+      url: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      age: 36,
+      note: 'Basketball Player',
+    },
+  ]);
 
   people.map((person) => {
     person.age;
@@ -20,6 +22,8 @@ function App() {
   return (
     <div className="App">
       <h1>V I </h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
