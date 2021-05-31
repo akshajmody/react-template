af0c4b4f60b02d929a8ed9c19e0996b3334dc2ac
Akshaj Mody <akshajmody@gmail.com>
2021-05-30 17:39:52 -0700

    basic TS example complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
 
+interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
 function App() {
-  const [number, setNumber] = useState(5);
-  return <div className="App"></div>;
+  const [people, setPeople] = useState<IState['people']>([]);
+
+  people.map((person) => {
+    person.age;
+  });
+
+  return (
+    <div className="App">
+      <h1>V I </h1>
+    </div>
+  );
 }
 
 export default App;
